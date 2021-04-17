2cebb15ce9b233eaf0049ed9fa0f2543d7264aca
Demir Demirel <demirdemirel@sabanciuniv.edu>
2021-04-18 01:38:02 +0300

    CHANGED profile page app bar

---
+++ lib/ui/components/RightDrawer/right_drawer_viewmodel.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class DrawerItemModel {
+  IconData icon;
+  String text;
+
+  DrawerItemModel({this.icon, this.text});
+}
+
+class RightDrawerViewModel extends ChangeNotifier {
+  int _counter = 0;
+  // int selectedIndex = 0;
+
+  // void onItemTapped(int index) {
+  //   selectedIndex = index;
+  //   notifyListeners(); // <-- notify listeners
+  // }
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners(); // <-- notify listeners
+  }
+}
